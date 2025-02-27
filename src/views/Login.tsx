6bde0ea3cb01289ad7a62cfbb90029eaec9a0758
mattpe <mattpe@metropolia.fi>
2025-02-27 10:28:15 +0200

    add register form

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import {Button} from '@rneui/base';
+import {ScrollView} from 'react-native';
 
 const Login = () => {
   const [displayRegister, setDisplayRegister] = useState(false);
@@ -11,14 +12,14 @@ const Login = () => {
   };
 
   return (
-    <>
-      {displayRegister ? <RegisterForm /> : <LoginForm />}
+    <ScrollView>
+      {displayRegister ? <RegisterForm setDisplayRegister={setDisplayRegister} /> : <LoginForm />}
         <Button
           onPress={toggleRegister}
         >
           or {displayRegister ? 'login' : 'register'}?
         </Button>
-    </>
+    </ScrollView>
   );
 };
 
