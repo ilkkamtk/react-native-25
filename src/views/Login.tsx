fb7fa6f5c657fbe1372a7e0ee7654d3310200e26
mattpe <mattpe@metropolia.fi>
2025-02-27 10:55:36 +0200

    login & register about done

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,24 +1,32 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import {Button} from '@rneui/base';
 import {ScrollView} from 'react-native';
+import {useUserContext} from '../hooks/ContextHooks';
 
 const Login = () => {
   const [displayRegister, setDisplayRegister] = useState(false);
-  
+  const {handleAutoLogin} = useUserContext();
+
   const toggleRegister = () => {
     setDisplayRegister(!displayRegister);
   };
 
+  useEffect(() => {
+    handleAutoLogin();
+  }, []);
+
   return (
     <ScrollView>
-      {displayRegister ? <RegisterForm setDisplayRegister={setDisplayRegister} /> : <LoginForm />}
-        <Button
-          onPress={toggleRegister}
-        >
-          or {displayRegister ? 'login' : 'register'}?
-        </Button>
+      {displayRegister ? (
+        <RegisterForm setDisplayRegister={setDisplayRegister} />
+      ) : (
+        <LoginForm />
+      )}
+      <Button onPress={toggleRegister}>
+        or {displayRegister ? 'login' : 'register'}?
+      </Button>
     </ScrollView>
   );
 };
